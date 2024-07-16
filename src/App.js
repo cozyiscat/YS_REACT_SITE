877314fd921504a6ca24773492f1cabae1e643d9
cozyiscat <yuiop7664@gmail.com>
2024-07-16 15:38:14 +0900

    컴포넌트 사용법

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-function Ul(){
-  return(
-    <ul>
-    <li>강아지</li>
-    <li>고양이</li>
-    <li>거북이</li>
-    <li>토끼</li>
-  </ul>
-  )
-}
-// 리액트 함수는 파스칼방식 (함수 앞글자 대문자)
-function App() {
-  return (
-    <div className="App">
-      <Ul></Ul>
 
-      <table border='1px'>
-        <tr>
-            <td>이름</td>
-            <td>나이</td>
-        </tr>
-        <tr>
-            <td>홍길동</td>
-            <td>24</td>
-        </tr>
-        <tr>
-            <td>이순신</td>
-            <td>60</td>
-        </tr>
-      </table>
-    </div>
-  );
-}
-
-export default App;
+export const App = ()=> {
+    const divStyle= {
+      border: '1px solid blue'
+    }
+    return (
+      <div style={divStyle}>
+      <p>안냥하세요</p>
+      <p>반갑습니다</p>
+      </div>
+    );
+  }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {App} from './App';
+// const App = ()=> {
+//   const divStyle= {
+//     border: '1px solid blue'
+//   }
+//   return (
+//     <div style={divStyle}>
+//     <p>안냥하세요</p>
+//     <p>반갑습니다</p>
+//     </div>
+//   );
+// }
+// ReactDOM.render(<App/>,document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <App/>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
